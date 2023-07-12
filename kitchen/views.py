382381d7fce3da9488c965509738587db7166c71
Glieb <gleblitvinenko3007@gmail.com>
2023-07-12 14:12:11 +0300

    Modified main page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from kitchen.models import Cook, Dish, DishType
 
-def index(response):
-    return render(response, "kitchen/index.html")
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1
+    }
+
+    return render(request, "kitchen/index.html", context=context)
