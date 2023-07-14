5dcf69b356eedfdf3420ca96bbd1f7def55336b9
Glieb <gleblitvinenko3007@gmail.com>
2023-07-14 14:59:01 +0300

    Added restaurant-kitchen-service-vqj9.onrender.com to ALLOWED_HOSTS

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://restaurant-kitchen-service-vqj9.onrender.com/"]
 
 LOGIN_REDIRECT_URL = "/"
 # Application definition
